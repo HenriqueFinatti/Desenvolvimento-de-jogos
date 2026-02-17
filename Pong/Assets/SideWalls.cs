72c63ff0a9c73b89588be5b61eb52a0e6974ca41
Henrique Finatti <finatti.henrique@gmail.com>
2026-02-17 13:57:48 -0300

    Finalizando o pong

---
+++ Pong/Assets/SideWalls.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+public class SideWalls : MonoBehaviour
+{
+    void OnTriggerEnter2D (Collider2D hitInfo) {
+        if (hitInfo.tag == "Ball")
+        {
+            string wallName = transform.name;
+            GameManager.Score(wallName);
+            hitInfo.gameObject.SendMessage("RestartGame", null, SendMessageOptions.RequireReceiver);
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
