80b3191d676790a24ec1b023fef9df07eb253ab0
Tiago Fagundes dos Santos <tiagofagundesdossantos@gmail.com>
2026-03-07 19:20:59 -0300

    Trazendo os inimigos em matriz e fazendo a movimentação deles

--- Space Invaders/main.lua
+++ Space Invaders/main.lua
@@ -7,6 +7,12 @@ local Player = require 'src.entities.Player'
 local Sprites = require 'src.utils.Sprites'
 local Enemy = require 'src.entities.Enemy'
 
+--Variaveis para definir o movimento da frota de inimigos
+local fleetDirection = 1 -- direção eixo x
+local fleetTimer = 0 
+local fleetDelay = 0.3
+
+--Variaveis para definir a janela do jogo
 local VIRTUAL_WIDTH = 225
 local VIRTUAL_HEIGHT = 300
 local WINDOW_WIDTH = 750
@@ -17,7 +23,29 @@ local function playerDefinitions()
 end
 
 local function enemyDefinitions()
-    EnemyShip = Enemy(VIRTUAL_WIDTH, VIRTUAL_HEIGHT)
+    --Definicao da posicao dos inimigos e a mtrzi representada por eles
+    Enemies = {}
+
+    local startX = 40
+    local startY = 40
+    local spacingX = 35
+    local spacingY = 25
+
+    for row = 1,4 do
+        Enemies[row] = {}
+
+        for col = 1,5 do
+            local enemy = Enemy(VIRTUAL_WIDTH, VIRTUAL_HEIGHT)
+
+            enemy.x = startX + (col-1) * spacingX
+            enemy.y = startY + (row-1) * spacingY
+
+            enemy.row = row
+
+            Enemies[row][col] = enemy
+        end
+    end
+
 end
 
 function love.load()
@@ -46,15 +74,66 @@ end
 function love.update(dt)
     PlayerShip:movePlayer(dt)
     PlayerShip:update(dt)
-    EnemyShip:update(dt)
+    --Movimentação do inimigo
+    for row=1,4 do
+        for col=1,5 do
+            Enemies[row][col]:update(dt)
+        end
+    end
+    fleetTimer = fleetTimer + dt
+
+    if fleetTimer >= fleetDelay then
+
+        fleetTimer = 0
+
+        local hitRight = false
+        local hitLeft = false
+
+        for row=1,4 do
+            for col=1,5 do
+                local enemy = Enemies[row][col]
+
+                enemy.x = enemy.x + fleetDirection
+
+                if enemy.x > VIRTUAL_WIDTH - 20 then
+                    hitRight = true
+                end
+
+                if enemy.x < 20 then
+                    hitLeft = true
+                end
+            end
+        end
+
+        if hitRight then
+            fleetDirection = -1
+        end
+
+        if hitLeft then
+            fleetDirection = 1
+
+            for row=1,4 do
+                for col=1,5 do
+                    Enemies[row][col].y =
+                    Enemies[row][col].y + 10
+                end
+            end
+        end
+
+    end
 end
 
 function love.draw()
+    --Renderiza o player e inimigos na tela
     Push:start()
 
     love.graphics.clear(10 / 255, 11 / 255, 26 / 255, 1)
 
     PlayerShip:render()
-    EnemyShip:render()
+    for row=1,4 do
+        for col=1,5 do
+            Enemies[row][col]:render()
+        end
+    end
     Push:finish()
 end
